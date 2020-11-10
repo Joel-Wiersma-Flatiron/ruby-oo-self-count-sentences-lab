91dcd2c75158e32b972fdab485b5e5730823e557
Joel-Wiersma-Flatiron <Joel.Wiersma.D@gmail.com>
2020-11-09 20:57:04 -0700

    commit tree 2ba18e057a476ff958dffb96acb7c91866f1f866

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return true if self[-1] == "."
+    return false
   end
 
   def question?
-
+    return true if self[-1] == "?"
+    return false
   end
 
   def exclamation?
-
+    return true if self[-1] == "!"
+    return false
   end
 
   def count_sentences
-
+    count = 0
+    previous_character = ""
+    i = 0
+    while i < self.length
+      count += 1 if self[i] == "." && previous_character != "."
+      count += 1 if self[i] == "?" && previous_character != "?"
+      count += 1 if self[i] == "!" && previous_character != "!"
+      previous_character = self[i]
+      i += 1
+    end
+    count
   end
 end
